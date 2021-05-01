a5eaa630be5317217428d3e186eed0f45dff5fa2
jifrozen <answldjs1836@naver.com>
2021-05-01 16:48:59 +0900

    section2-Spring Cloud Gateway

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,14 +1,47 @@
 package com.example.firstservice;
 
+
+import com.netflix.discovery.converters.Auto;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
-    @GetMapping("welcome")
+
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env){
+        this.env=env;
+    }
+
+    @GetMapping("/welcome")
     public String  welcome(){
         return "Welcome to the First service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in First Service.";
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request){
+        log.info("Server port={}",request.getServerPort());
+
+        return String.format("Hi, there. This is a message from first Service on PORT %s"
+                ,env.getProperty("local.server.port"));
     }
 }
